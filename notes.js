288aa4ecff109903d072db57b9afaa24e3a30fca
Ayush Singh <ayushsops@gmail.com>
2021-07-08 19:56:23 +0530

    Refactoring

--- notes.js
+++ notes.js
@@ -1,34 +1,40 @@
 const fs = require('fs')
 const chalk = require('chalk') 
 
-const getNotes = function(){
+const getNotes = ()=>{
     return 'Your Notes...'
 }
 
-const addNote = function(title, body){
+const listNotes = ()=>{
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function (note){
-        return note.title === title
+    console.log(chalk.inverse('Your Notes...'))
+    notes.forEach((note)=>{
+        console.log(note.title)
     })
+}
 
-    if(duplicateNotes.length===0){
+const addNote =(title, body)=>{
+    const notes = loadNotes()
+    const duplicateNote = notes.find((note)=>note.title === title)
+
+    if(!duplicateNotes){
         notes.push({
             title:title,
             body:body
         })
         saveNotes(notes)
-        console.log('New Note Added!')
+        console.log(chalk.green.inverse('New Note Added!'))
     }else{
-        console.log('Note title taken :(')
+        console.log(chalk.red.inverse('Note title taken :('))
     }
 }
 
-const saveNotes = function(notes){
+const saveNotes =(notes)=>{
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function(){
+const loadNotes =()=>{
     try{
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString()
@@ -38,12 +44,10 @@ const loadNotes = function(){
     }
 }
 
-const removeNote = function(title){
+const removeNote = (title)=>{
     const notes = loadNotes()
-    const notesToKeep = notes.filter(function(note){
-        return note.title !==title
-    })
-    if(notes.length>notesToKeep){
+    const notesToKeep = notes.filter((note)=>note.title !== title) 
+    if(notes.length>notesToKeep.length){
         console.log(chalk.green.inverse('Note Removed!'))
         saveNotes(notesToKeep)
     }else{
@@ -51,8 +55,14 @@ const removeNote = function(title){
     }
 }
 
+const readNote = (title) =>{
+    
+}
+
 module.exports = {
     getNotes:getNotes, 
     addNote:addNote,
-    removeNote:removeNote
+    removeNote:removeNote,
+    listNotes:listNotes,
+    readNote:readNote
 }
